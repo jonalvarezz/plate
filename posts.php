7bc3e8f2e4374e6e19d567adbfad24532f2c7fa9
Jona <jonas.al.co@gmail.com>
2013-10-01 15:56:10 -0500

    better way to markup figure within article. now image is clicklable from blog home

--- posts.php
+++ posts.php
@@ -6,14 +6,14 @@
 		<ul class="post-entries">
 			<?php while( posts() ): ?>
 				<li class="post clickable" id="article-<?php echo article_id(); ?>">
-					<?php // Featured Image ?>
-					<?php if ( article_custom_field('thumbnail') ) : ?>
-						<figure class="feat-image strip text_center" style="background-color: <?php echo article_custom_field('thumbnail-bg', '#f9f9f9'); ?> !important;">
-							<img src="<?php echo article_custom_field('thumbnail'); ?>" alt="<?php echo article_title(); ?>">
-						</figure>
-					<?php endif; ?>
 					<article class="post-entry">
 						<header>
+							<?php // Featured Image ?>
+							<?php if ( article_custom_field('thumbnail') ) : ?>
+								<figure class="feat-image strip text_center" style="background-color: <?php echo article_custom_field('thumbnail-bg', '#f9f9f9'); ?> !important;">
+									<img src="<?php echo article_custom_field('thumbnail'); ?>" alt="<?php echo article_title(); ?>">
+								</figure>
+							<?php endif; ?>
 							<h2 class="post-title">
 								<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
 							</h2>
